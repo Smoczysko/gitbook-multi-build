7bfa59029cace0ff06f8ff0e388db38ee6848afa
Łukasz Rybka <darkstariw@gmail.com>
2019-01-23 00:06:02 +0100

    Initial project structure.

---
+++ bin/index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const program = require('commander');
+const chalk = require('chalk');
+
+const pkg = require(path.join(__dirname, '../package.json'));
+
+program
+    .version(pkg.version)
+    .description(pkg.description);
+
+program.on('command:*', () => {
+    console.error(chalk.red('Invalid command: ', program.args.join(' ')) + '\n');
+    console.error(chalk.red('See --help for a list of available commands.') + '\n');
+
+    process.exit(1);
+});
+
+if (!process.argv.slice(2).length) {
+    console.warn(chalk.yellow('No command specified!' + '\n'));
+
+    program.outputHelp(help => chalk.yellow(help));
+
+    process.exit(1);
+}
+
+program.parse(process.argv);
+
